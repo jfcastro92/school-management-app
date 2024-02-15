fa0e6ddd4e0f5f1aa2c72e3602c3f887bbce4662
Juan Fernando Castro M. <jfcastro9208@gmail.com>
2024-02-14 19:55:57 -0500

    :tada: School Management App initial commit - NestJS MySQL

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { AppController } from './controller/app.controller';
+import { AppService } from './service/app.service';
+import { StudentModule } from './modules/student.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'localhost',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    StudentModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/entities/studentCourse/studentToCourse.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, Column, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Student } from '../student/student.entity';
+import { Course } from '../course/course.entity';
+
+@Entity()
+export class StudentToCourse {
+  @PrimaryGeneratedColumn()
+  public studentToCourseId: number;
+
+  @Column()
+  public studentId: number;
+
+  @Column()
+  public courseId: number;
+
+  @Column()
+  public grade: number;
+
+  @ManyToOne(() => Student, (student: Student) => student.studentToCourses)
+  public student: Student;
+
+  @ManyToOne(() => Course, (course: Course) => course.courseToStudent)
+  public course: Course;
+}
